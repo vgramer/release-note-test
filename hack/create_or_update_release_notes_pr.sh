4d9b2e50a6e0d051ce05694cf0f65ac8f483447a
Vincent Gramer <vgramer@gmail.com>
2021-03-24 20:44:06 +0100

    add log
    Signed-off-by: Vincent Gramer <vgramer@gmail.com>

--- hack/create_or_update_release_notes_pr.sh
+++ hack/create_or_update_release_notes_pr.sh
@@ -15,23 +15,33 @@ release_note_branch='update-release-notes'
 pr_number="$(hub pr list -h update-release-notes --format '%I')"
 
 if [[ -z "${pr_number}" ]]; then
+  echo "release notes pr does note already exist"
   git checkout -b "${release_note_branch}"
 else
+  echo "release notes pr already exist (#pr_number)"
   hub pr checkout "${pr_number}"
   git reset --hard master
 fi
 
+echo "=================================================================================================================="
+echo "=================================================================================================================="
+echo  "generating next release notes"
 
 ${ROOT}/hack/generate_release_notes.sh
 
+echo "=================================================================================================================="
+echo "=================================================================================================================="
+echo  "release notes succesfully generated"
+echo  "merging next release notes with changelog"
+
+
 echo -e "\n" >> next-release-notes.md
 cat next-release-notes.md CHANGELOG.md > tmp_changelog.md
-mv tmp_changelog CHANGELOG.md
+mv tmp_changelog.md CHANGELOG.md
 
 git add CHANGELOG.md
 git commit -m "update changelog with new version"
 
+echo "create or update PR"
 hub pull-request --file release-notes-pr-description.md --force
-
-
 
